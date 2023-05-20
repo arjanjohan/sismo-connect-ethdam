186279405fb7a2dfc3eea86e71e2d2727e98fd5d
arjanjohan <arjanvanhoogdalem@gmail.com>
2023-05-20 16:17:47 +0200

    wagmi wont load

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
 import BackButton from "@/components/BackButton";
 import {
+  switchNetwork,
+  mumbaiFork,
+  getPublicClient,
+  handleVerifyErrors,
+  callContract,
+  signMessage,
+  publicWalletClient,
+} from "@/utils";
+import { createWalletClient, http, custom, WalletClient, PublicClient, parseEther } from "viem";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+// import { abi } from "../../abi/AirdropLevel0.json";
+
+import {
   SismoConnectButton,
   SismoConnectClientConfig,
   SismoConnectResponse,
@@ -23,10 +36,35 @@ type UserType = {
   id: string;
 };
 
-export default function Level0RegisterUser() {
+export enum APP_STATES {
+  init,
+  receivedProof,
+  purchasing,
+  success,
+}
+
+// The application calls contracts on Mumbai testnet
+const userChain = mumbaiFork;
+const contractAddress = "";
+// TODO: Remplace with merchant address
+
+
+export default function RegisterTwitterUser() {
+  const [appState, setAppState] = useState<APP_STATES>(APP_STATES.init);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [contractError, setContractError] = useState<string>("");
   const [verifiedUser, setVerifiedUser] = useState<UserType>(null);
+  const [walletClient, setWalletClient] = useState<WalletClient>(
+    createWalletClient({
+      chain: userChain,
+      transport: http(),
+    }) as WalletClient
+  );
+
+  const publicClient: PublicClient = getPublicClient(userChain);
+
+  const { address, isConnected } = useAccount();
 
   async function verify(response: SismoConnectResponse) {
     // first we update the react state to show the loading state
@@ -52,6 +90,30 @@ export default function Level0RegisterUser() {
     } finally {
       // We set the loading state to false to show the user profile
       setLoading(false);
+    }
+  }
+
+  async function claimWithSismo(responseBytes: string) {
+    setAppState(APP_STATES.purchasing);
+    // switch the network
+    await switchNetwork(userChain);
+    const abi = null; //TODO: REMOVE THIS
+    try {
+      const tokenId = await callContract({
+        contractAddress,
+        responseBytes,
+        abi,
+        userChain,
+        address: address as `0x${string}`,
+        publicClient,
+        walletClient,
+      });
+      // If the proof is valid, we update the user react state to show the tokenId
+      setAppState(APP_STATES.success);
+    } catch (e) {
+      setContractError(handleVerifyErrors(e));
+    } finally {
+      setAppState(APP_STATES.init);
     }
   }
 
